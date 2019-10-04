c14e7b2534328cad3e0820be3ff3e658a8c5ae83
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-04 09:35:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,7 +53,8 @@ end
 def reduce_to_all_true(array)
   counter = 0 
   while counter < array.size do 
-    return false if(counter == false) # current element is false(condition)
+    if(counter == false) # current element is false 
+    return false 
     counter += 1 
   end 
   return true 
